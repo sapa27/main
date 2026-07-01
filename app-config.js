640a5e4c8a98d0d1a111af925677f3166fd4336a
sapa27 <natthapong.sripa@gmail.com>
2026-07-01 08:59:55 +0700

    Update app-config.js

--- app-config.js
+++ app-config.js
@@ -11,7 +11,7 @@
   }
   var defaults = {
     appTitle: 'ระบบบริหารจัดการเรื่องพิจารณา',
-    gasWebAppUrl: '',
+    gasWebAppUrl: 'https://script.google.com/macros/s/AKfycbwSgyDJZNIgykDSUnM4EX1CC4dHeAr8WUEgf8aUkqJkz76QUJ2MH7Zxq-wEm0PREx3gKQ/exec',
     logoUrl: FALLBACK_LOGO,
     fallbackLogoUrl: FALLBACK_LOGO,
     localAssetBase: './partials/',
