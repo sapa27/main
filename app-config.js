50029dc0d2504f179eb8c0170c37023658857f16
sapa27 <natthapong.sripa@gmail.com>
2026-07-14 19:59:25 +0700

    Update app-config.js

--- app-config.js
+++ app-config.js
@@ -5,9 +5,7 @@
   var ASSET_STAMP = "asset-manifest-commission-v1.2-github-pages-gas-direct-2026-07-14-r115";
   var APP_VERSION = "1.2.0-production-current";
   var DEFAULT_GAS_WEB_APP_URL = [
-    "https://script.google.com/macros/s/",
-    "AKfycbzt3p-NLOg8QpmnB_Bj03Rds6H9SlNevnbcOAqzm1vzuAFXPtXhYVlDUTblCclmjSAm",
-    "/exec"
+    "https://script.google.com/macros/s/AKfycbw1Pi4dJiQyiVuvcCdEi5IkzEG9A2Y8Dm9S-ue5prBBjIXmpl54Pd7oqnBkWZPaKNmy/exec"
   ].join("");
   var DEFAULT_LOGO_URL = [
     "https://upload.wikimedia.org/wikipedia/commons/",
