e9269a392df5877da03bab1696f2f78e3115b46a
sapa27 <natthapong.sripa@gmail.com>
2026-07-16 19:40:15 +0700

    Update app-config.js

--- app-config.js
+++ app-config.js
@@ -6,7 +6,7 @@
   var APP_VERSION = "1.2.0-production-current";
   var DEFAULT_GAS_WEB_APP_URL = [
     "https://script.google.com/macros/s/",
-    "AKfycbyQZcetvUPxA8OI_vWGiBV2fRT3G3Gkqpho443kX79GQMFJ3eSbL2RDSYYg7S10J4c",
+    "AKfycbzUqf1T5UhU1852RhcdrOWboIrzHqzBIENC4Ddr1bBQBJBb6-Gh4tJedyXcztXImxoj",
     "/exec"
   ].join("");
   var DEFAULT_LOGO_URL = [
